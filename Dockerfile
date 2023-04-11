33182dd6cfddd1a4b51974464e0be710bbc05343
Suresh <suresh.nanavath@dcmail.ca>
2023-04-10 23:32:25 -0400

    new commit

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM python:3.8-slim-buster
 WORKDIR /code
 COPY requirements.txt /code
 RUN pip install -r requirements.txt --no-cache-dir
-COPY . /code 
-CMD python app.py
+COPY . /code
+EXPOSE $PORT
+CMD python app.py --port $PORT --host 0.0.0.0
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import joblib
 import sklearn
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -41,4 +42,5 @@ def predict():
         return render_template('home.html', prediction=None)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port='5000')
+    port=os.environ.get("PORT",5000)
+    app.run(host='0.0.0.0',port=port,debug=False)
